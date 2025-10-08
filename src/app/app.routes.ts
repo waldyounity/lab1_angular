0fcd9837ca3c500a61f5338be66adc32e5d66d77
waldyounity <waldhur@gmail.com>
2025-10-07 21:28:56 -0400

    actualizando

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,24 @@
 import { Routes } from '@angular/router';
+import { Layout } from './layout/layout';
+import path from 'path';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+
+    {
+        path:'',
+        component:Layout,
+        children:[
+            {
+                path:'',
+                loadComponent:()=>
+                    import('./home/home').then(m=>m.Home)
+            },
+            {
+                path:'about',
+                loadComponent:()=>
+                    import('./about/about').then(m=>m.About)
+            }
+        ]
+    }
+
+];
